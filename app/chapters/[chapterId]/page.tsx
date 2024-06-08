56ec7ca9d76514571312b99ff50ce379cb1d2c0e
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-08 16:36:27 +0530

    moved to burda container for adding meta

--- app/chapters/[chapterId]/page.tsx
+++ app/chapters/[chapterId]/page.tsx
@@ -1,7 +1,12 @@
 import React, { useRef, useState } from "react";
 import BurdaContainer from "@/components/BurdaContainer/BurdaContainer";
 
-export async function generateMetadata({ params }) {
+interface GenerateMetadataParams {
+  params: {
+    [key: string]: any; // Adjust this as per your specific route parameters
+  };
+}
+export async function generateMetadata({ params }: GenerateMetadataParams) {
   return {
     title: `Qaseeda Burdah - Chapter ${params.chapterId}`,
     description: `Read Qaseeda Burdah Chapter ${params.chapterId} along with audio, translations and tasfeer`,
