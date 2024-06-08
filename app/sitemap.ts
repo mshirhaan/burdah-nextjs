b22bc1e0f5259f0a084c4de4b4b0cf200cde63c8
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-08 22:47:19 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteMaps: MetadataRoute.Sitemap = [
+    {
+      url: "https://qaseedaburda.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+
+  for (let i = 1; i <= 10; i++) {
+    siteMaps.push({
+      url: `https://qaseedaburda.com/chapters/${i}`,
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.8,
+    });
+  }
+  return siteMaps;
+}
