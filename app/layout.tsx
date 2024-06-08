53583c04d584b327a3b42ce00004def50a90454a
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-08 23:29:50 +0530

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,29 @@ const myFont = localFont({
   display: "swap",
 });
 
-console.log("mm", myFont);
-
 export const metadata: Metadata = {
-  title: "Qaseeda Burdah",
+  title: "Qaseeda Burda - Translations, Tafseer, and Audio Recitations",
   description:
-    "Read the chapters of Qaseeda Burdah along with Audios and Translations.",
+    "Explore Qaseeda Burda with English translations, detailed tafseer explanations, and synchronized audio recitations. Enjoy a rich, interactive experience of this timeless Islamic poem.",
+  keywords:
+    "Qaseeda, Qasida, Qaseeda Burda, Qasida Burda, Qasida Burdah, Qaseeda Burdah, Burdah, Burda, Qasidat al-Burdah, Imam al-Busiri, Islamic poetry, Prophet Muhammad, spiritual poems, Islamic literature, religious poems, devotional poetry, translations, tafseer, audio recitations",
+  authors: [{ name: "Imam al-Busiri" }],
+
+  openGraph: {
+    title: "Qaseeda Burda - Translations, Tafseer, and Audio Recitations",
+    description:
+      "Discover the Qaseeda Burda with English translations, detailed tafseer, and synchronized audio recitations. Experience the poem like never before.",
+    url: "https://www.qaseedaburda.com",
+    type: "website",
+    images: ["https://www.qaseedaburda.com/images/logo.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Qaseeda Burda - Translations, Tafseer, and Audio Recitations",
+    description:
+      "Explore the Qaseeda Burda with English translations, detailed tafseer explanations, and synchronized audio recitations.",
+    images: ["https://www.qaseedaburda.com/images/logo.png"],
+  },
 };
 
 export default function RootLayout({
