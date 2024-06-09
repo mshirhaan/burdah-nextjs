9ac060e07f9cf10d00502dcab22e84a4595c9c64
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-09 21:56:01 +0530

    fixed typo

--- components/QaseedaIntroduction/QaseedaIntroduction.tsx
+++ components/QaseedaIntroduction/QaseedaIntroduction.tsx
@@ -6,15 +6,15 @@ const QaseedaIntroduction = () => {
       <section>
         <h2>Introduction</h2>
         <p>
-          The Burdah, or the Prophet&apos;s{" "}
+          The Burdah, or the Prophet&apos;s
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
-          </span>{" "}
+          </span>
           Mantle, is a Qasidah (poem) composed by the great Sufi Shaykh Imam
-          Sharafuddin Muhammad Al-Busiri{" "}
+          Sharafuddin Muhammad Al-Busiri
           <span lang="ar" dir="rtl">
-            عليهالرمحة
-          </span>{" "}
+            عليه الرحمة
+          </span>
           who was born in 608 A.H. (1212 C.E) and passed away in 695 A.H. (1296
           C.E).
         </p>
@@ -23,16 +23,16 @@ const QaseedaIntroduction = () => {
       <section>
         <h2>History</h2>
         <p>
-          Imam Busiri{" "}
+          Imam Busiri
           <span lang="ar" dir="rtl">
-            عليهالرمحة
-          </span>{" "}
+            عليه الرحمة
+          </span>
           wrote this Qasidah after having suffered a stroke and being paralysed
           in half his body. After compiling this Qasidah, he went to sleep. He
-          was granted the sacred opportunity of seeing the Beloved Prophet{" "}
+          was granted the sacred opportunity of seeing the Beloved Prophet
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
-          </span>{" "}
+          </span>
           in his dream, who placed His sacred hands over his body and covered
           him with His sacred shawl. Instantly, he was cured. When he woke up,
           he observed that he was able to stand and move about.
@@ -44,19 +44,19 @@ const QaseedaIntroduction = () => {
         <p>
           The next day, when he came out of his house, he met a Dervish (Holy
           person) who was a stranger to him. The Dervish asked him to recite the
-          Qasidah, which he had written in praise of the Holy Prophet{" "}
+          Qasidah, which he had written in praise of the Holy Prophet
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
           </span>
           . He asked the Dervish which Qasidah he was referring to. He said the
-          one you recited in front of The Most Beloved{" "}
+          one you recited in front of The Most Beloved
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
-          </span>{" "}
+          </span>
           in the blessed court last night. The news of this spread far and wide.
           Hence, the Qasidah came to be called Qasidah tu&apos;l Burdah and
           received veneration among all Muslims as a Qasidah especially approved
-          by the Beloved Prophet{" "}
+          by the Beloved Prophet
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
           </span>
@@ -93,7 +93,7 @@ const QaseedaIntroduction = () => {
           you have just read, the author of this Qasidah gained great benefits.
           This Qasidah will continue to be heard and recited till the Day of
           Judgement, with the same devotion and reverence. In’sha’Allah, it will
-          provide spiritual relief to the servants of the Beloved Prophet{" "}
+          provide spiritual relief to the servants of the Beloved Prophet
           <span lang="ar" dir="rtl">
             صلى الله عليه وسلم
           </span>
