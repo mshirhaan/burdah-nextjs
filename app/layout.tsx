f72674e6613af8f7ed5c16e1883cad7472fa8790
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-10 10:42:27 +0530

    fixed player

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,9 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
-      </head>
       <body className={montserrat.className}>
         <ThemeProvider>
           <AutoPlayProvider>{children}</AutoPlayProvider>
