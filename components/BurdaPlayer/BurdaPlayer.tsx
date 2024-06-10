9f4513d61e95752d228dd8892c9471493e4b62c4
Mohammed Shirhaan <mohammedshirhaan@Mohammeds-MacBook-Pro.local>
2024-06-10 09:09:59 +0530

    fixed player

--- components/BurdaPlayer/BurdaPlayer.tsx
+++ components/BurdaPlayer/BurdaPlayer.tsx
@@ -14,7 +14,6 @@ function BurdaPlayer({
   selectedReciter,
 }: any) {
   const [isHidden, setIsHidden] = useState(false);
-  const [isMuted, setIsMuted] = useState(false);
   const { theme } = useTheme();
 
   const isScrollAtEnd = () => {
@@ -25,13 +24,6 @@ function BurdaPlayer({
     let subtractValue = 100;
 
     return windowHeight + scrollPosition >= documentHeight - subtractValue;
-  };
-
-  const toggleMute = () => {
-    if (audioRef.current) {
-      audioRef.current.muted = !isMuted;
-      setIsMuted(!isMuted);
-    }
   };
 
   const handleScroll = () => {
@@ -52,27 +44,6 @@ function BurdaPlayer({
     };
   }, []);
 
-  const handleProgressBarClick = (e: React.MouseEvent<HTMLProgressElement>) => {
-    if (audioRef.current) {
-      const progressBar = e.currentTarget;
-      const clickX = e.clientX - progressBar.getBoundingClientRect().left;
-      const percent = (clickX / progressBar.offsetWidth) * 100;
-      const duration = audioRef.current.duration;
-      const newTime = (duration * percent) / 100;
-
-      audioRef.current.currentTime = newTime;
-      setPlaybackTime(newTime);
-    }
-  };
-
-  const handleSliderChange = (value: [number]) => {
-    const audio = audioRef.current;
-    if (audio) {
-      audio.currentTime = value[0];
-      setPlaybackTime(value[0]);
-    }
-  };
-
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.addEventListener("timeupdate", () => {
@@ -81,25 +52,12 @@ function BurdaPlayer({
     }
   }, []);
 
-  // Function to format time in HH:MM:SS format
-  // const formatTime = (timeInSeconds: number): string => {
-  //   const hours = Math.floor(timeInSeconds / 3600);
-  //   const minutes = Math.floor((timeInSeconds % 3600) / 60);
-  //   const seconds = Math.floor(timeInSeconds % 60);
-
-  //   const paddedMinutes = minutes.toString().padStart(2, "0");
-  //   const paddedSeconds = seconds.toString().padStart(2, "0");
-
-  //   return `${hours}:${paddedMinutes}:${paddedSeconds}`;
-  // };
-
   return (
     <div
       className={`${styles.container} ${
         isHidden || !audioUrl ? styles.hidden : ""
       } ${theme == "dark" ? styles.dark : ""}`}
     >
-      {/* Dropdown for selecting the reciter */}
       <select value={selectedReciter} onChange={handleReciterChange}>
         {reciters.map((reciterName: string) => (
           <option key={reciterName} value={reciterName}>
@@ -116,34 +74,6 @@ function BurdaPlayer({
       >
         Your browser does not support the audio element.
       </audio>
-      {/* <div className={styles.player}>
-        <button
-          onClick={togglePlay}
-          className={`${theme == "dark" ? "dark" : ""}`}
-        >
-          {isPlaying ? (
-            <ThemeIcon Icon={FaPause} />
-          ) : (
-            <ThemeIcon Icon={FaPlay} />
-          )}
-        </button>
-        <button
-          onClick={toggleMute}
-          className={`${theme == "dark" ? "dark" : ""}`}
-        >
-          {isMuted ? (
-            <ThemeIcon Icon={FaVolumeMute} />
-          ) : (
-            <ThemeIcon Icon={FaVolumeUp} />
-          )}
-        </button>
-        <progress
-          max={audioRef.current?.duration || 0}
-          value={currentTime}
-          style={{ width: "100%", cursor: "pointer" }}
-          onClick={handleProgressBarClick}
-        ></progress>
-      </div> */}
     </div>
   );
 }
